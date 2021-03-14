71ad11b27c6134a486132420ac8da328ddfb9f0f
juanmuscaria <juanmuscaria@gmail.com>
2021-03-14 20:13:02 -0300

    Don't assume we are the first one in the classloader, people may used some wrapper taking over the order.

--- src/main/java/io/github/crucible/Crucible.java
+++ src/main/java/io/github/crucible/Crucible.java
@@ -4,6 +4,8 @@ import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
 import org.bukkit.plugin.Plugin;
 
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.Properties;
 
 public class Crucible {
@@ -15,18 +17,31 @@ public class Crucible {
         boolean parsedIsDevBuild = false;
         int forgeBuild = 0;
         try {
-            Properties manifest = new Properties();
-            manifest.load(Crucible.class.getResourceAsStream("/META-INF/MANIFEST.MF"));
-            parsedVersion = manifest.getProperty("Implementation-Version");
-            parsedIsDevBuild = parsedVersion.contains("dev");
-            Properties fmlversion = new Properties();
-            fmlversion.load(Crucible.class.getResourceAsStream("/fmlversion.properties"));
-            forgeBuild = Integer.parseInt(String.valueOf(fmlversion.getProperty(
-                    "fmlbuild.build.number", "0")));
+            Enumeration<URL> resources = Crucible.class.getClassLoader()
+                    .getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                Properties manifest = new Properties();
+                manifest.load(url.openStream());
+                if (manifest.getProperty("Forge-Version") == null)
+                    continue;
+                parsedVersion = manifest.getProperty("Implementation-Version", parsedVersion);
+                parsedIsDevBuild = parsedVersion.contains("dev");
+                forgeBuild = Integer.parseInt(System.getProperty("thermos.forgeRevision", "0"));
+                if (forgeBuild == 0) {
+                    Properties fmlversion = new Properties();
+                    fmlversion.load(Crucible.class.getResourceAsStream("/fmlversion.properties"));
+                    forgeBuild = Integer.parseInt(String.valueOf(fmlversion.getProperty(
+                            "fmlbuild.build.number", "0")));
+                }
+                break;
+            }
         } catch (Exception e) {
             FMLLog.severe("[Crucible] Unable to parse metadata.");
             e.printStackTrace();
         }
+        if (forgeBuild == 0)
+            throw new RuntimeException("Unable to parse forge version");
         CRUCIBLE_VERSION = parsedVersion;
         IS_DEV_BUILD = parsedIsDevBuild;
         FORGE_BUILD_VERSION = forgeBuild;
