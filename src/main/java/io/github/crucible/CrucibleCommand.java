b6f115e6e760ecaeb6c8dc39ca2e5aa1ec82a71d
Juan Patricio <juanmuscaria@gmail.com>
2022-09-26 13:40:07 -0300

    Implemented crucible mod container and plugin

---
+++ src/main/java/io/github/crucible/CrucibleCommand.java
@@ -0,0 +1,259 @@
+package io.github.crucible;
+
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.ModContainer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldProvider;
+import net.minecraftforge.cauldron.CauldronHooks;
+import net.minecraftforge.common.DimensionManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.spigotmc.RestartCommand;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class CrucibleCommand extends Command {
+
+    private static final DecimalFormat timeFormat = new DecimalFormat("########0.000");
+    private static WeakReference<MinecraftServer> serveReference;
+
+    protected CrucibleCommand(MinecraftServer server) {
+        super("crucible");
+        serveReference = new WeakReference<>(server);
+
+        StringBuilder usage = new StringBuilder();
+        usage.append("&7&m-------------------&7[&bCrucible&7]&m-------------------\n");
+        usage.append("&b  >&e crucible tps &7-&a Show tps statistics.\n");
+        usage.append("&b  >&e crucible restart &7-&a Restart the server.\n");
+        usage.append("&b  >&e crucible info &7-&a Print some information about the server.\n");
+        usage.append("&b  >&e crucible chunks &7-&a Print some information about loaded chunks.\n");
+        usage.append("&b  >&e crucible heap &7-&a Dump the server heap.\n");
+        usage.append("&b  >&e crucible plugins &7-&a Shows all your loaded plugins and mod plugins.\n");
+        usage.append("&b  >&e crucible mods &7-&a Shows all your loaded mods.\n");
+        setUsage(ChatColor.translateAlternateColorCodes('&', usage.toString()));
+        setPermission("crucible");
+    }
+
+    public static String generateInfo() {
+        StringBuilder info = new StringBuilder();
+        info.append("This server is running &3Crucible&r [").append(CrucibleModContainer.instance.getVersion()).append("] (Thermos fork by CrucibleMC Team).\n");
+        info.append("&9https://github.com/CrucibleMC/Crucible\n&r");
+        info.append("Bukkit API implemented: 1.7.9-R0.3-SNAPSHOT\n");
+        info.append("Plugins: ").append(Bukkit.getPluginManager().getPlugins().length).append("\n&r");
+        info.append("Mods: ").append(Loader.instance().getActiveModList().size())
+                .append(" &r| Loaded: ").append(Loader.instance().getModList().size()).append("\n&r");
+        return ChatColor.translateAlternateColorCodes('&' ,info.toString());
+    }
+
+    private static String getPluginList() {
+        StringBuilder pluginList = new StringBuilder();
+        Plugin[] plugins = Bukkit.getPluginManager().getPlugins();
+
+        for (Plugin plugin : plugins) {
+            if (pluginList.length() > 0) {
+                pluginList.append(ChatColor.WHITE);
+                pluginList.append(", ");
+            }
+
+            if (CrucibleModContainer.isModPlugin(plugin))
+                pluginList.append(ChatColor.AQUA);
+            else
+                pluginList.append(plugin.isEnabled() ? ChatColor.GREEN : ChatColor.RED);
+
+            pluginList.append(plugin.getDescription().getName()).append("@").append(plugin.getDescription().getVersion());
+        }
+
+        return "(" + plugins.length + "): " + pluginList.toString();
+    }
+
+    private static String getModList() {
+        StringBuilder modList = new StringBuilder();
+
+        List<ModContainer> mods = Loader.instance().getModList();
+
+        for (ModContainer mod : mods) {
+            if (modList.length() > 0) {
+                modList.append(ChatColor.WHITE);
+                modList.append(", ");
+            }
+
+            modList.append(ChatColor.GREEN).append(mod.getName()).append("@").append(mod.getVersion());
+        }
+
+        return "(" + mods.size() + "): " + modList.toString();
+    }
+
+    private static String getTps() {
+        StringBuilder tps = new StringBuilder();
+        tps.append("\n&8[&e&l\u26a1&r&8] &7TPS from last 1m, 5m, 15m: &r");
+
+        double[] recentTps = MinecraftServer.getServer().recentTps;
+
+        for (double t : recentTps) {
+            tps.append(parseTps(t));
+        }
+
+        double meanTickTime = mean(getServer().tickTimeArray) * 1.0E-6D;
+        tps.append("\n&r&8[&e&l\u26a1&r&8]&7 Mean tick time: &l").append(timeFormat.format(meanTickTime)).append("&r&7ms&r\n");
+
+        for (Integer dimId : DimensionManager.getIDs()) {
+            double worldTickTime = mean(getServer().worldTickTimes.get(dimId)) * 1.0E-6D;
+            double worldTPS = Math.min(1000.0 / worldTickTime, 20);
+            WorldProvider worldProvider = DimensionManager.getProvider(dimId);
+            String name = worldProvider.getDimensionName();
+            if (name.equals("Overworld")) {
+                name = worldProvider.worldObj.getSaveHandler().getWorldDirectoryName();
+            }
+            tps.append("&8(&2&l")
+                    .append(dimId)
+                    .append(" &r&7&o\u279c &r&b")
+                    .append(name).append("&r&8) &7Time: &l")
+                    .append(timeFormat.format(worldTickTime))
+                    .append("&r&7ms TPS: ")
+                    .append(parseTps(worldTPS))
+                    .append("&r\n");
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', tps.toString());
+    }
+
+    private static String parseTps(double tps) {
+        StringBuilder parsedTps = new StringBuilder();
+        if (tps <= 10)
+            parsedTps.append("&c&l");
+        else if (tps <= 15)
+            parsedTps.append("&e&l");
+        else
+            parsedTps.append("&a&l");
+
+        parsedTps.append(String.format("%.2f", Math.min(Math.round(tps * 100.0) / 100.0, 20))).append("&r ");
+        return parsedTps.toString();
+    }
+
+    /*
+     * Based on ForgeCommand
+     */
+    private static long mean(long[] values) {
+        long sum = 0L;
+        for (long v : values) {
+            sum += v;
+        }
+
+        return sum / values.length;
+    }
+
+    /*
+     * Based on ForgeCommand
+     */
+    private static MinecraftServer getServer() {
+        return serveReference.get();
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (!testPermission(sender))
+            return true;
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.BLUE + "[Crucible] " + ChatColor.GRAY + "Please specify action");
+            sender.sendMessage(usageMessage);
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("tps")) {
+            if (!testPermission(sender, "crucible.tps"))
+                return true;
+            sender.sendMessage(getTps());
+        } else if (args[0].equalsIgnoreCase("restart")) {
+            if (!testPermission(sender, "crucible.restart"))
+                return true;
+            RestartCommand.restart(true);
+        } else if (args[0].equalsIgnoreCase("info")) {
+            if (!testPermission(sender, "crucible.info"))
+                return true;
+            sender.sendMessage(generateInfo());
+        } else if (args[0].equalsIgnoreCase("chunks")) {
+            if (!testPermission(sender, "crucible.chunks"))
+                return true;
+            processChunks(sender, args);
+        } else if (args[0].equalsIgnoreCase("heap")) {
+            if (!testPermission(sender, "crucible.heap"))
+                return true;
+            processHeap(sender, args);
+        } else if (args[0].equalsIgnoreCase("mods")) {
+            if (!testPermission(sender, "crucible.mods"))
+                return true;
+            sender.sendMessage("Mods " + getModList());
+        } else if (args[0].equalsIgnoreCase("plugins")) {
+            if (!testPermission(sender, "crucible.plugins"))
+                return true;
+            sender.sendMessage("Plugins " + getPluginList());
+        } else if (args[0].equalsIgnoreCase("")) {
+            if (!testPermission(sender, "crucible."))
+                return true;
+        } else {
+            sender.sendMessage(ChatColor.RED + "Unknown subcommand.");
+            sender.sendMessage(usageMessage);
+        }
+        return true;
+    }
+
+    public boolean testPermission(CommandSender target, String permission) {
+        if (testPermissionSilent(target, permission)) {
+            return true;
+        }
+        target.sendMessage(ChatColor.BLUE + "[Crucible] " + ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is an error.");
+        return false;
+    }
+
+    public boolean testPermissionSilent(CommandSender target, String permission) {
+        if (!super.testPermissionSilent(target)) {
+            return false;
+        }
+        for (String p : permission.split(";"))
+            if (target.hasPermission(p))
+                return true;
+        return false;
+    }
+
+    private void processHeap(CommandSender sender, String[] args) {
+        File file = new File(new File(new File("."), "dumps"), "heap-dump-" + new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss").format(new Date()) + "-server.hprof");
+        sender.sendMessage("Writing heap dump to: " + file);
+        CauldronHooks.dumpHeap(file, true);
+        sender.sendMessage("Heap dump complete.");
+    }
+
+    private void processChunks(CommandSender sender, String[] args) {
+        sender.sendMessage(ChatColor.GOLD + "Dimension stats: ");
+        for (net.minecraft.world.WorldServer world : MinecraftServer.getServer().worlds) {
+            sender.sendMessage(ChatColor.GOLD + "Dimension: " + ChatColor.GRAY + world.provider.dimensionId +
+                    ChatColor.GOLD + " Loaded Chunks: " + ChatColor.GRAY + world.theChunkProviderServer.loadedChunkHashMap_KC.size() +
+                    ChatColor.GOLD + " Active Chunks: " + ChatColor.GRAY + world.activeChunkSet.size() +
+                    ChatColor.GOLD + " Entities: " + ChatColor.GRAY + world.loadedEntityList.size() +
+                    ChatColor.GOLD + " Tile Entities: " + ChatColor.GRAY + world.loadedTileEntityList.size()
+            );
+            sender.sendMessage(ChatColor.GOLD + " Entities Last Tick: " + ChatColor.GRAY + world.entitiesTicked +
+                    ChatColor.GOLD + " Tiles Last Tick: " + ChatColor.GRAY + world.tilesTicked +
+                    ChatColor.GOLD + " Removed Entities: " + ChatColor.GRAY + world.unloadedEntityList.size() +
+                    ChatColor.GOLD + " Removed Tile Entities: " + ChatColor.GRAY + world.field_147483_b.size()
+            );
+        }
+
+        if ((args.length < 2) || !"dump".equalsIgnoreCase(args[1])) {
+            return;
+        }
+        boolean dumpAll = ((args.length > 2) && "all".equalsIgnoreCase(args[2]));
+
+        File file = new File(new File(new File("."), "chunk-dumps"), "chunk-info-" + new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss").format(new Date()) + "-server.txt");
+        sender.sendMessage("Writing chunk info to: " + file);
+        CauldronHooks.writeChunks(file, dumpAll);
+        sender.sendMessage("Chunk info complete");
+    }
+
+}
---
+++ src/main/java/io/github/crucible/CrucibleModContainer.java
@@ -0,0 +1,251 @@
+package io.github.crucible;
+
+
+import com.avaje.ebean.EbeanServer;
+import com.google.common.collect.ImmutableList;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import net.minecraft.server.MinecraftServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Event;
+import org.bukkit.event.Listener;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.*;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.*;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("UnstableApiUsage")
+public class CrucibleModContainer extends DummyModContainer implements Plugin {
+    public static Logger logger = LogManager.getLogger("Crucible");  //Crucible - note: use it only after forge configured the logger!
+    public static CrucibleModContainer instance;
+    public static Metrics metrics;
+    private PluginLoader dummyPluginLoader;
+    private PluginDescriptionFile dummyPluginDescription;
+    private boolean isPluginEnabled = false;
+    private java.util.logging.Logger pluginLogger;
+
+    public CrucibleModContainer() {
+        super(new ModMetadata());
+        ModMetadata meta = getMetadata();
+        meta.modId = "Crucible";
+        meta.name = "Crucible Server";
+        meta.version = CrucibleMetadata.CRUCIBLE_VERSION;
+        meta.credits = "TODO: Add credits";
+        meta.authorList = Arrays.asList("juanmuscaria", "brunoxkk0", "evernife");
+        meta.description = "Pure black magic and gambiarras!";
+        meta.url = "https://github.com/CrucibleMC/Crucible";
+        instance = this;
+    }
+
+    @Override
+    public boolean registerBus(EventBus bus, LoadController controller) {
+        bus.register(this);
+        return true;
+    }
+
+    @Subscribe
+    public void modConstruction(FMLConstructionEvent evt) {
+        NetworkRegistry.INSTANCE.register(this, this.getClass(), "*", evt.getASMHarvestedData());
+        logger.info("Crucible DummyMod injected successfully!");
+    }
+
+    @Subscribe
+    public void preInit(FMLPreInitializationEvent evt) {
+        //No usage needed for now.
+    }
+
+    @Subscribe
+    public void postInit(FMLPostInitializationEvent evt) {
+        //No usage needed for now.
+    }
+
+    @Subscribe
+    public void onAvailable(FMLLoadCompleteEvent evt) {
+        //No usage needed for now.
+    }
+
+    @Subscribe
+    public void serverStarting(FMLServerStartingEvent evt) {
+        evt.registerServerCommand("crucible", new CrucibleCommand(evt.getServer()));
+        getServer().getPluginManager().injectModPlugin(this);
+        getServer().getPluginManager().enablePlugin(this);
+        metrics = new Metrics(this, 6555);
+    }
+
+    @Override
+    public List<String> getOwnedPackages() {
+        return ImmutableList.of(
+                "io.github.crucible.bootstrap",
+                "io.github.crucible"
+        );
+    }
+
+    @Override
+    public File getDataFolder() {
+        return new File(((File) MinecraftServer.options.valueOf("plugins")), "forge");
+    }
+
+    @Override
+    public PluginDescriptionFile getDescription() {
+        if (dummyPluginDescription == null) {
+            dummyPluginDescription = new PluginDescriptionFile(CrucibleModContainer.instance.getName(), CrucibleModContainer.instance.getVersion(), CrucibleModContainer.class.getName());
+        }
+        return dummyPluginDescription;
+    }
+
+    @Override
+    public FileConfiguration getConfig() {
+        return null;
+    }
+
+    @Override
+    public InputStream getResource(String filename) {
+        return null;
+    }
+
+    @Override
+    public void saveConfig() {
+        //Dummy plugin.
+    }
+
+    @Override
+    public void saveDefaultConfig() {
+        //Dummy plugin.
+    }
+
+    @Override
+    public void saveResource(String resourcePath, boolean replace) {
+        //Dummy plugin.
+    }
+
+    @Override
+    public void reloadConfig() {
+        //Dummy plugin.
+    }
+
+    @Override
+    public PluginLoader getPluginLoader() {
+        if (dummyPluginLoader == null) {
+            dummyPluginLoader = new PluginLoader() {
+                @Override
+                public Plugin loadPlugin(File file) throws InvalidPluginException, UnknownDependencyException {
+                    try {
+                        return getServer().getPluginManager().loadPlugin(file);
+                    } catch (InvalidDescriptionException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                public PluginDescriptionFile getPluginDescription(File file) {
+                    if (dummyPluginDescription == null) {
+                        dummyPluginDescription = new PluginDescriptionFile(CrucibleModContainer.instance.getName(), CrucibleModContainer.instance.getVersion(), CrucibleModContainer.class.getName());
+                    }
+                    return dummyPluginDescription;
+                }
+
+                @Override
+                public Pattern[] getPluginFileFilters() {
+                    return new Pattern[0];
+                }
+
+                @Override
+                public Map<Class<? extends Event>, Set<RegisteredListener>> createRegisteredListeners(Listener listener, Plugin plugin) {
+                    return null;
+                }
+
+                @Override
+                public void enablePlugin(Plugin plugin) {
+                    isPluginEnabled = true;
+                }
+
+                @Override
+                public void disablePlugin(Plugin plugin) {
+                    if (!MinecraftServer.getServer().isServerRunning()) isPluginEnabled = false;
+                }
+            };
+        }
+        return dummyPluginLoader;
+    }
+
+    @Override
+    public Server getServer() {
+        return Bukkit.getServer();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isPluginEnabled;
+    }
+
+    @Override
+    public void onDisable() {
+        //Dummy plugin.
+    }
+
+    @Override
+    public void onLoad() {
+        getLogger().info("Crucible DummyPlugin injected successfully!");
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public boolean isNaggable() {
+        return false;
+    }
+
+    @Override
+    public void setNaggable(boolean canNag) {
+    }
+
+    @Override
+    public EbeanServer getDatabase() {
+        return null;
+    }
+
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return null;
+    }
+
+    @Override
+    public java.util.logging.Logger getLogger() {
+        if (pluginLogger == null)
+            pluginLogger = new PluginLogger(this);
+        return pluginLogger;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return Collections.emptyList();
+    }
+
+    public static boolean isModPlugin(Plugin plugin) {
+        return plugin.getClass().getClassLoader().equals(Loader.instance().getModClassLoader()) ||
+                plugin.getClass().getClassLoader().equals(CrucibleModContainer.class.getClassLoader());
+    }
+}
