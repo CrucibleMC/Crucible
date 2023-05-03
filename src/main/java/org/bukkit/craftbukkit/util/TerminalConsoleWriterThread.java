10ca983ceeddcc415e32911b13ed5de9b574c053
Juan Patricio <juanmuscaria@gmail.com>
2023-05-03 12:13:53 -0300

    Improved logging.
    * Now everything is logged as soon as the server starts, no more holding logs until bukkit initialization.
    * Now loggers follow the forge style of logging, this also applies to plugins
    * Added crucible option to show who is logging to System.out, now you can catch that annoying mod/plugin spamming nonsense in the console.
    * Added log4j context values for System.out calls, you can use %X{crucible-className} %X{crucible-methodName} and %X{crucible-lineNumber} on custom formatted logs for more precise information on what is directly printing to your logs
    * Added plugin prefix to colored console sender messages when logStdOutCaller is enabled
    
     This change can cause breaking issues to logging related things. More testing may be required.

--- src/main/java/org/bukkit/craftbukkit/util/TerminalConsoleWriterThread.java
+++ src/main/java/org/bukkit/craftbukkit/util/TerminalConsoleWriterThread.java
@@ -6,6 +6,9 @@ import net.minecraft.server.MinecraftServer;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,6 +19,17 @@ public class TerminalConsoleWriterThread implements Runnable {
     public TerminalConsoleWriterThread(OutputStream output, ConsoleReader reader) {
         this.output = output;
         this.reader = reader;
+        // Crucible start - drain the logger since it will have duplicated content
+        try {
+            Field queues = QueueLogAppender.class.getDeclaredField("QUEUES");
+            queues.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            Map<String, BlockingQueue<String>> instance = (Map<String, BlockingQueue<String>>) queues.get(null);
+            instance.get("TerminalConsole").clear();
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
+        // Crucible end
     }
 
     public void run() {
