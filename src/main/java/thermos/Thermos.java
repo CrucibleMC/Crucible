48e59e834295e279d019852cf4a0c3ed9f4df941
Gravita <12893402+gravit0@users.noreply.github.com>
2026-06-12 02:36:20 +0700

    Fix EndlessIDs compatibility

---
+++ src/main/java/thermos/Thermos.java
@@ -0,0 +1,8 @@
+package thermos;
+
+// EndlessID check this class for thermos compatibility
+// https://github.com/GTMEGA/EndlessIDs/blob/a780cde306e2d049d52eca07c444d95ce87f8399/src/main/java/com/falsepattern/endlessids/mixin/plugin/Mixin.java#L629
+public class Thermos {
+    private Thermos() {
+    }
+}
